fa4db07ea24a39eb0289d09498cf3d0da174a8c0
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-10 22:54:54 -0300

    sidebar/main/footer separados

--- Projeto Spotify/tailwind-spotify/src/app/page.tsx
+++ Projeto Spotify/tailwind-spotify/src/app/page.tsx
@@ -1,9 +1,17 @@
 export default function Home() {
   return (
-    <main>
-
-      <h1>Hello world</h1>
-      
-    </main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-950 p-6">
+          sidebar
+        </aside>
+        <main className="flex-1 p-6">
+          main
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
+    </div>
   )
 }
